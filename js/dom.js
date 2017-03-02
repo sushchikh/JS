d65dec396a1bf2f78b799c8a75e947cb44fb5602
sushchikh <a.sushchikh@gmail.com>
2017-03-02 17:58:00 +0300

    add translate.js + dom.js

--- js/dom.js
+++ js/dom.js
@@ -33,3 +33,6 @@ console.log('только дочерние "элеметны" body');
 for (var i = 0; i < document.body.children.length; i++){
 	console.log(document.body.children[i]);
 };
+
+//доступ к body, дальше можно плясать внутрь и в бок
+//console.log(document.documentElement.firstElementChild.nextElementSibling); -> <body>
