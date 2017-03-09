3da063e97a30101ba5ae5201b128addfda5ab209
sushchikh <a.sushchikh@gmail.com>
2017-03-09 14:03:44 +0300

    Add files via upload

--- jQuery/js/my.js
+++ jQuery/js/my.js
@@ -1,4 +1,8 @@
-$(document).ready(function(){
+﻿// устаревшая (depricated) запись запуска скрипта
+//$(document).ready(function(){
+	
+// современная запись	
+$(function(){	
 	
 
 	var link = $('.features_link');// ловим элмент
@@ -12,62 +16,214 @@ $(document).ready(function(){
 
 
 
-	var listElement = $('li'); // âûáîð âñåõ li íà ñòðàíèöå
-	var liHeader = listElement.filter( '.header' ); // ôèëüòð âûáèðàåò òîëüêî ýëåìåíòû èç li ñ êëàññîì main
-	var notHeader = listElement.not( '.header' ); // âûáèðàåì ýëåìåòíû ÁÅÇ êëàññà header
-	var hasSpans = listElement.has( 'span' ); // âûáîð ýëåìåíòîâ li, èìåþùèõ âíòóðè <span>
+	var listElement = $('li'); // ловим все 'li'
+	var liHeader = listElement.filter( '.header' ); // среди всех ли ловим содержащие класс ".header"
+	var notHeader = listElement.not( '.header' ); // ловим все "li" не содержащие класс ".header"
+	var hasSpans = listElement.has( 'span' ); // ловим все "li" имеющие внутри <span>
 	
 	console.log(listElement);
 	console.log(liHeader);
 	console.log(notHeader);
 	console.log(hasSpans);
+
+
 	
 	
-	// êàðàáêàåìñÿ ïî DOM-äåðåâó:
+///// карабкаемся по DOM-дереву: //////////////////////////////////////////////////////////////////////////
 	
-	// Ïîëó÷èòü ïåðâûé ïóíêò ñïèñêà íà ñòðàíèöå
-	var listItem = $( 'li' ).first(); // è .last()
+	// среди всех "li" выбираем первый 
+	var listItem = $( 'li' ).first(); // и .last()
   
-	// Ïîëó÷èòü ðîäñòâåííûå ýëåìåíòû ýòîãî ïóíêòà ñïèñêà
+	// соседи по дереву
 	var siblings = listItem.siblings();
 	  
-	// Ïîëó÷èòü ñëåäóþùåãî ðîäñòâåííèêà ýòîãî ïóíêòà ñïèñêà
-	var nextSibling = listItem.next(); // è .prev()
+	// следующий сосед
+	var nextSibling = listItem.next(); // И .prev() предыдущий
 	  
-	// Ïîëó÷èòü ðîäèòåëÿ ýòîãî ïóíêòà ñïèñêà
+	// родитель элемента
 	var list = listItem.parent();
 
-	// Ïîëó÷èòü ïóíêòû ñïèñêà ÿâëÿþùèõñÿ íåïîñðåäñòâåííûìè ïîòîìêàìè list
+	// дети элемента
 	var listItems = list.children();
 
-	// Ïîëó÷èòü ÂÑÅ ïóíêòû ñïèñêà èç list, âêëþ÷àÿ âëîæåííûå
+	// содержащие <span>
 	var allListItems = list.find( 'span' );
 
-	// Íàéòè âñåõ ïðåäêîâ ýòîãî ïóíêòà ñïèñêà ñ êëàññîì module
+	// родилети с классом .module
 	var modules = listItem.parents( '.module' );
 
-	// Íàéòè áëèæàéøåãî ïðåäêà ýòîãî ïóíêòà ñïèñêà ñ êëàññîì module
+	// ближайщие к имеющему класс ".module"
 	var module = listItem.closest( '.module' );
 	
-	console.log('');
+	var someElse = $('.box .wrapper'); //  элементы с классом box и wrapper
+	var someElse = $('.box').find('.wrapper'); // тоже, что выше, только работает чуть быстрее
+	
+	var someElse2 = $('div.wrapper'); //  только дивы с классом wrapper
+
+	var someElse3 = $('h1, h2'); //  h1 и h2
+	
+	var someElse4 = $('div article h2'); //  h2, которые в article, которые в div
+	
+	var someElse5 = $('div + p'); // все "p"  перед которыми идет "div"
+	
+	var someElse6 = $('#someId ~ p'); // все "р" перед которыми идет элемент с id "someId"
+	
+	var someElse7 = $('.someClass').next(); //$('.someClass').prev() -- следующий/предыдущий элемент за элементом с классом ".someClass"
+	
+	var someElse8 = $('*'); // все элементы
+	
+	var someElse9 = $('.someClass > h2'); // все h2, являющиеся непосредственными потомками элементов с классом ".someClass"
+	
+	var someElse10 = $('.someClass > *'); // все потомки элементов с классом ".someClass"
+	
+	var someElse11 = $('.someClass').children(); //все потомки элементов с классом ".someClass"
 	
 	
-	// element.addClass('class'); -- äîáàâëåíèå êëàññà
-	// element.removeClass('class'); -- óäàëåíèå êëàññà
-	// element.toggleClass('class'); -- ïåðåêëþ÷åíèå êëàññà (åñëè íåò — äîáàâèò, åñëè åñòü — óáåðåò)
+// поиск по атрибутам ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+	
+	var searchAtrr = $('a[href]'); // все ссылки с атрибутом href
+	
+	var searchAtrr2 = $('a[href=\\#]'); // все ссылки на id
+	
+	var searchAtrr3 = $('a[href~=\\#]'); // все ссылки у которых в href есть ссылка на id
+	
+	var searchAtrr4 = $('a[herf^=http]'); // все ссылки начинающиеся на http
+	
+	var searchAtrr5 = $('a[href*=google\\.com]'); // ссылки имеющие в себе google.com
+	
+	// !IMPORTANT - надо экранировать (\\ двумя обратными слешами) специальные символы, чтобы jquery не ругался
 	
 	
+// атрибуты элементов и CSS//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	var attr = $("#my").css('color'); // получаем значение цвета шрифта
+	$('attr').css('color', 'red'); // устанавливаем значение цвета шрифта
+	// устанавливаем сразу несколько значений
+	$('attr').css({
+		'color':'red',
+		'font-size':'14px',
+		'margin-left':'10px'
+		});
+		
+	
+// манипуляции с классами ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+
+	$('someElement').addClass('className someOneClass'); // добавляем классы
+	$('someElement').hasClass('className'); // проверяет есть ли у элемента заданый класс
+	$('someElement').removeClass('className'); // удаляем класс
+	$('someElement').toggleClass('className'); //включаем/выключаем класс у элемента
+	
+
+// манипуляции	с атрибутами (<a href='атрибут' alt="атрибут" и т.д.>)
+
+	var atrr = $('someElement').attr('alt'); // получаем значение атрибута alt у элемента
+	$('attr').attr('scr', '../img/default.png'); // меняем значение атрибута src
+	$('attr').removeAttr('alt'); // удалили атрибут alt
+	
+	
+	
+// события / events
+
+	$('.list_for_test li').click(function(event){
+		console.log('do smth');
+		event.preventDefault(); // блокируем стандартное действие (если оно было, например если мы клацаем по ссылке, чтобы не случилось перехода)
+		event.stopPropagation(); // блокируем "всплывание в цепочке DOM-дерева", чтобы можно было обрабатывать события по клику на родитлея
+});
+	
+	$('.list_for_test li').click(function(event){
+		console.log('do smth else');
+		event.stopImmediatePropagation(); // сработает только этот обработчик собтия
+		return false; // блокируем стандартное действие + блокируем "всплывание в цепочке DOM-дерева" (альетратива варианту выше)
+});
+	
+	$('.someClass').off(); // удалить обработчик объекта
+	
+
+// Touch-события ####################################
+
+	// touchstart - событие при касании пальцем экрана
+	// tiuchend - убираем палец с экрана
+	// touchmove - водим пальцем по экрану
+	// touchcancel - отмена "touch" до того, как палец убран
+	
+
+
+// Animations
+	//зарезервированные слова "slow" и "fast" определяют скорсоть анимации в милисекундах, можно просто задавать скорость в мс в скобках после функции
+
+	$('.someClass').hide(600); // скрыть (display: none)
+	$('.someClass').show(400); // показать
+	$('.someClass').toggle(600); // скрыть/показать используя заданную скорость
+
+	$('.someClass').hide(600, function(){  // добавим к этому колл-бэк функцию, которая выполниться, когда элемент скроется
+		console.log('объект скрылся'); 
+	});
+
+
+	$('.someClass').slideUp(500); // свернуть элемент (создание баяна), также возможен коллбэк
+	$('.someClass').slideDown(500); // развернуть элемент, также возможен коллбэк
+	$('.someClass').slideToggle(600); // развернуть/свернуть , также возможен коллбэк
+
+	$('.someClass').fadeIn(500); // меняет opacity от 0 до предыдущего значения
+	$('.someClass').fadeOut(500); // меняет opacity от текущего значения до 0
+	$('.someClass').fadeToggle(500); // перключатель между fadeIn и fadeOut
+	$('.someClass').fadeTo(500, 0.3); // меняет opacity до указанного значения
+
+	
+
+
+
+
+// TEST AREA############################################################
+
+
+
+//// обработка клик-функция ///////////////////////////////////////////////////////////////////////
 	
 	// $( 'li' ).click(function( event ) {
 	// 	console.log( 'clicked', $( this ).text() );
 	// });
 	
 	$('button').click(function(event) {
-		$('span').hide();
+		var redElement = $('#tools').css('color');
+		$('redElement').css('color', 'red');
+		//$('span').css('color', 'blue');
+		//console.log($('.features_link').hasClass('show'));
+		$('li').slideToggle(600, function(){  // добавим к этому колл-бэк функцию, которая выполниться, когда элемент скроется
+			console.log('объект скрылся'); 
+		});
 	});
 	
 	
 	
 	
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
