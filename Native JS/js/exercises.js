64f4e4859712df08c5b82752eb2a8648b65ce6d1
sushchikh <a.sushchikh@gmail.com>
2017-03-18 15:15:42 +0300

    + exercises htm, js, css. Release simply carousel

---
+++ Native JS/js/exercises.js
@@ -0,0 +1,10 @@
+﻿var carousels = document.querySelectorAll('#carousel_container .carousel_element');
+var currentCarousel = 0;
+var carouselInteraval = setInterval(nextCarousel, 5000);
+
+function nextCarousel(){
+	carousels[currentCarousel].className = 'carousel_element';
+	currentCarousel = (currentCarousel+1) % carousels.length;
+	carousels[currentCarousel].className = 'carousel_element show'
+};
+
