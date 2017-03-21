e6c254932da0b54a2e516981c565c9948ceaed00
sushchikh <a.sushchikh@gmail.com>
2017-03-21 18:05:43 +0300

    arrays перебор

--- Native JS/js/arrays_exercises.js
+++ Native JS/js/arrays_exercises.js
@@ -1,2 +1,41 @@
 "use strict"
-11
+
+// forEach
+var firstArray = [1,2,3,4,5,6,7,8,9, -1, -2, -3, -4];
+var secondArray= [];
+firstArray.forEach( function(element) {
+	secondArray.push(element * element);
+});
+console.log(secondArray);
+
+
+// map
+firstArray = firstArray.map(function(elem) {
+	return elem*elem;
+});
+console.log(firstArray);
+
+
+// filter
+var thirdArray = [1,2,3,4,5,6,7,9, -1, -2, -3, -4]
+thirdArray = thirdArray.filter(function(elem) {
+	return elem < 0;
+	// return !(index % 2);
+});
+console.log(thirdArray);
+
+// some
+var fourthArray = []
+for(var i = -5; i < 11; i++){
+	fourthArray.push(i);
+};
+
+fourthArray = fourthArray.some(function(elem){ 
+	return elem > 0; // вернет true, если хотыб один из элементов больше 0
+});
+console.log(fourthArray);
+
+// every
+fourthArray = fourthArray.every(function(elem){
+	return elem > 0; // вернет true, если все элементы больше ноля (=> false)
+});
